9170300f20c5e5d407e3fbfafb0b9cdb83546932
Cshark <caleb.c.sharkey@gmail.com>
2020-08-13 23:47:43 +0100

    Add files via upload

---
+++ Startup.py
@@ -0,0 +1,14 @@
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix = "os.")
+
+@bot.event
+async def on_ready():
+    print("Bot ready")
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send(f"Pong: {round(bot.latency * 1000)}ms")
+
+bot.run("NzQzNTgwODA3NDIzMTk3MzA0.XzWvlw.laqRWR_iI108xgTISGIvl4M6Vbo")
