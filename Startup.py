004b010f1dc91d7d0951afa3dbd3cb10d7a46e6b
Cshark <caleb.c.sharkey@gmail.com>
2020-08-14 03:55:55 +0100

    Update Startup.py

--- Startup.py
+++ Startup.py
@@ -1,14 +1,32 @@
 import discord
 from discord.ext import commands
+import os
+import json
 
-bot = commands.Bot(command_prefix = "os.")
+def get_prefix(client, message):
+    with open("prefixes.json", "r") as pref:
+        prefixes = json.load(pref)
+
+    return prefixes[str(message.guild.id)]
+
+bot = commands.Bot(command_prefix = get_prefix)
 
 @bot.event
 async def on_ready():
     print("Bot ready")
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send(f"Pong: {round(bot.latency * 1000)}ms")
+@bot.event
+async def on_guild_join(guild):
+    with open("prefixes.json", "r") as pref:
+        prefixes = json.load(pref)
+
+    prefixes[str(guild.id)] = "os."
+
+    with open("prefixes.json", "w") as pref:
+        json.dump(prefixes, pref)
+
+for file in os.listdir("./modules"):
+    if file.endswith(".py"):
+        bot.load_extension(f"modules.{file[:-3]}")
 
 bot.run("#token")
