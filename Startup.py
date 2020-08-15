d9c1780847604a4486e767f257c75079fbb9dd4f
Cshark <caleb.c.sharkey@gmail.com>
2020-08-15 01:13:25 +0100

    Update Startup.py

--- Startup.py
+++ Startup.py
@@ -24,6 +24,14 @@ async def on_guild_join(guild):
 
     with open("prefixes.json", "w") as pref:
         json.dump(prefixes, pref)
+        
+@bot.event
+async def on_command_error(ctx, error):
+    if is_instance(error, commands.MissingRequiredArgument):
+        await ctx.send("Missing argument")
+    else:
+        with open(f"{time.time()}.txt","w") as error_file:
+            error_file.write(error)
 
 for file in os.listdir("./modules"):
     if file.endswith(".py"):
